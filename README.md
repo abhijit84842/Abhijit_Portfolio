230e6b40d5bd6875d6a8c328ddd08ac67e7da5c1
Abhijit Das <abhijitd721201@gmail.com>
2024-10-17 12:30:29 +0530

    app setup and create navbar

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1>Abhijit's Portfolio Website</h1>
+
+<h2>Technology Use </h2>
+* Next Js
+* Tailwind Css
---
+++ app/components/NavBar/Nav.js
@@ -0,0 +1,28 @@
+const NavBar = () => {
+  return (
+    <div className="flex justify-between mt-2">
+      <div>
+        <h2 className="ml-20 text-2xl font-semibold">Abhijit</h2>
+      </div>
+      <nav className="flex gap-20 mr-20 text-xl font-semibold">
+        <div>
+          Home
+        </div>
+        <div>
+          About Me
+        </div>
+        <div>
+          Skills
+        </div>
+        <div>
+          Projects
+        </div>
+        <div>
+          Contact
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
