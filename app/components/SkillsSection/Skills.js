53bbd1bbd2b566b244e3e9020c428eda215674ec
Abhijit Das <abhijitd721201@gmail.com>
2024-10-24 13:15:59 +0530

    skills sec completed and design update

--- app/components/SkillsSection/Skills.js
+++ app/components/SkillsSection/Skills.js
@@ -6,35 +6,35 @@ const Skills = () => {
       <h1 className="text-[#FB2A2A] flex justify-center text-4xl font-semibold my-20">
         Skills
       </h1>
-      <div className="skills-type bg-stone-700 mx-20">
+      <div className="skills-type bg-stone-700 mx-20 rounded-md">
         <div className="tech-heading mt-5 p-5">
-          <h2 className="text-[#FB2A2A] text-3xl">Technical Skills</h2>
+          <h2 className="text-[#FB2A2A] text-3xl font-semibold">Technical Skills</h2>
         </div>
 
-        <div className="programming-lang bg-zinc-800 p-5 my-10 mx-5 ">
+        <div className="programming-lang bg-zinc-800 p-5 my-10 mx-5 rounded-lg">
           <h3 className="text-2xl text-white font-semibold">
             Programming Languages
           </h3>
           <div className="prolang  flex justify-start gap-20 mt-5">
-            <div className="python p-5 flex flex-col items-center gap-2">
+            <div className="python p-5 flex flex-col items-center gap-4">
               <img
-                className="w-[3rem]"
+                className="w-[4rem]"
                 src="/skills/python 1.png"
                 alt="loading.."
               />
               <p className="text-white text-lg font-semibold">Python</p>
             </div>
-            <div className="java p-5 flex flex-col items-center gap-2">
+            <div className="java p-5 flex flex-col items-center gap-4">
               <img
-                className="w-[4rem]"
+                className="w-[5rem]"
                 src="/skills/java 1.png"
                 alt="loading.."
               />
               <p className="text-white text-lg font-semibold">Core Java</p>
             </div>
-            <div className="javascript p-5 flex flex-col items-center gap-2">
+            <div className="javascript p-5 flex flex-col items-center gap-4">
               <img
-                className="w-[3rem]"
+                className="w-[4rem]"
                 src="/skills/javascript 1.png"
                 alt="loading.."
               />
@@ -43,7 +43,7 @@ const Skills = () => {
           </div>
         </div>
 
-        <div className="programming-lang bg-zinc-800 p-5 my-10 mx-5 ">
+        <div className="programming-lang bg-zinc-800 p-5 my-10 mx-5 rounded-lg">
           <h3 className="text-2xl text-white font-semibold">
             Frontend Technology
           </h3>
@@ -119,30 +119,30 @@ const Skills = () => {
           </div>
         </div>
 
-        <div className="programming-lang bg-zinc-800 p-5 my-10 mx-5 ">
+        <div className="programming-lang bg-zinc-800 p-5 my-10 mx-5 rounded-lg">
           <h3 className="text-2xl text-white font-semibold">
             Backend Technology
           </h3>
           <div className="nodejs  flex justify-start gap-20 mt-5">
-            <div className="python p-5 flex flex-col items-center gap-2">
+            <div className="python p-5 flex flex-col items-center gap-3">
               <img
-                className="w-[3rem]"
-                src="/skills/node 1.png"
+                className="w-[5rem]"
+                src="/skills/node js 1.png"
                 alt="loading.."
               />
               <p className="text-white text-lg font-semibold">Node Js</p>
             </div>
-            <div className="express-js p-5 flex flex-col items-center gap-2">
+            <div className="express-js p-5 flex flex-col items-center gap-6">
               <img
-                className="w-[4rem]"
+                className="w-[5.5rem]"
                 src="/skills/express 1.png"
                 alt="loading.."
               />
               <p className="text-white text-lg font-semibold">Express Js</p>
             </div>
-            <div className="jwt-auth p-5 flex flex-col items-center gap-2">
+            <div className="jwt-auth p-5 flex flex-col items-center gap-7">
               <img
-                className="w-[3rem]"
+                className="w-[5rem]"
                 src="/skills/jwt 1.png"
                 alt="loading.."
               />
@@ -151,18 +151,18 @@ const Skills = () => {
           </div>
         </div>
 
-        <div className="programming-lang bg-zinc-800 p-5 my-10 mx-5 ">
+        <div className="programming-lang bg-zinc-800 p-5 my-10 mx-5 rounded-lg">
           <h3 className="text-2xl text-white font-semibold">Database</h3>
           <div className="prolang  flex justify-start gap-20 mt-5">
             <div className="mongoDB p-5 flex flex-col items-center gap-2">
               <img
-                className="w-[3rem]"
+                className="w-[5rem]"
                 src="/skills/mongoDB 1.png"
                 alt="loading.."
               />
               <p className="text-white text-lg font-semibold">mongoDB</p>
             </div>
-            <div className="mySQL p-5 flex flex-col items-center gap-2">
+            <div className="mySQL p-5 flex flex-col items-center gap-5">
               <img
                 className="w-[4rem]"
                 src="/skills/MYSQL.png"
@@ -173,20 +173,20 @@ const Skills = () => {
           </div>
         </div>
 
-        <div className="programming-lang bg-zinc-800 p-5  mb-10 mx-5 ">
+        <div className="programming-lang bg-zinc-800 p-5  mb-10 mx-5 rounded-lg">
           <h3 className="text-2xl text-white font-semibold">UI Design</h3>
           <div className="prolang  flex justify-start gap-20 mt-5">
             <div className="figma p-5 flex flex-col items-center gap-2">
               <img
-                className="w-[3rem]"
+                className="w-[5rem]"
                 src="/skills/figma 1.png"
                 alt="loading.."
               />
               <p className="text-white text-lg font-semibold">Figma</p>
             </div>
-            <div className="canva p-5 flex flex-col items-center gap-2">
+            <div className="canva p-5 flex flex-col items-center gap-3">
               <img
-                className="w-[4rem]"
+                className="w-[5rem]"
                 src="/skills/canva 1.png"
                 alt="loading.."
               />
@@ -195,10 +195,10 @@ const Skills = () => {
           </div>
         </div>
         <div className=" mt-10  p-5 ">
-          <div className="p-5">
-            <h2 className="text-[#FB2A2A] text-3xl">Professional Skills</h2>
+          <div className="p-2 ">
+            <h2 className="text-[#FB2A2A] text-3xl font-semibold">Professional Skills</h2>
           </div>
-          <div className="bg-zinc-800 flex justify-start gap-20 my-5 p-5">
+          <div className="bg-zinc-800 flex justify-start gap-20 my-5 p-5 rounded-lg">
             <div className=" p-5 flex flex-col items-center gap-5">
               <img
                 className="w-[6rem]"
