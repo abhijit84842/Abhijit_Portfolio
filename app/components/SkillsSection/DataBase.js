246b020f221995738618436f2251449755104b8d
Abhijit Das <abhijitd721201@gmail.com>
2025-02-15 23:02:06 +0530

    make database comp mobile responsive

--- app/components/SkillsSection/DataBase.js
+++ app/components/SkillsSection/DataBase.js
@@ -3,9 +3,38 @@ import React from "react";
 
 const DataBase = () => {
   return (
-    <div className="programming-lang bg-zinc-800 p-5 my-10 mx-5 rounded-lg">
-      <h3 className="text-2xl text-white font-semibold">Database</h3>
-      <div className="prolang  flex justify-start gap-20 mt-5">
+    <div className="programming-lang bg-zinc-800 p-2 my-5 mx-2 rounded-lg 2xl:my-10 2xl:mx-5 2xl:p-5">
+      <h3 className="text-xl text-white font-semibold 2xl:text-2xl">Database</h3>
+
+
+
+      <div className="prolang  flex justify-start flex-wrap gap-5 mt-5 2xl:hidden">
+        <div className="mongoDB p-2 flex flex-col items-center gap-6">
+          <Image
+            src="/Skills/mongodbskills.png"
+            alt="loading.."
+            loading="lazy"
+            width={50}
+            height={50}
+          />
+
+          <p className="text-white text-sm font-semibold">mongoDB</p>
+        </div>
+        <div className="mySQL p-2 flex flex-col items-center gap-5">
+          <Image
+            src="/Skills/MYSQLskills.png"
+            alt="loading.."
+            loading="lazy"
+            width={50}
+            height={50}
+          />
+
+          <p className="text-white text-sm font-semibold">MYSQL</p>
+        </div>
+      </div>
+
+
+      <div className="prolang hidden 2xl:flex justify-start gap-20 mt-5">
         <div className="mongoDB p-5 flex flex-col items-center gap-6">
           <Image
             src="/Skills/mongodbskills.png"
