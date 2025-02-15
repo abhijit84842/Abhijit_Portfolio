2e7c8230850d85385a82121acf8a84c8ec634c95
Abhijit Das <abhijitd721201@gmail.com>
2025-02-15 23:19:15 +0530

    make responsive ui design skills

--- app/components/SkillsSection/UiDesignSkills.js
+++ app/components/SkillsSection/UiDesignSkills.js
@@ -3,9 +3,37 @@ import React from "react";
 
 const UiDesignSkills = () => {
   return (
-    <div className="programming-lang bg-zinc-800 p-5  mb-10 mx-5 rounded-lg">
-      <h3 className="text-2xl text-white font-semibold">UI Design</h3>
-      <div className="prolang  flex justify-start gap-20 mt-5">
+    <div className="programming-lang bg-zinc-800 p-2  my-5 mx-2 rounded-lg 2xl:my-10 2xl:mx-2 2xl:p-5">
+      <h3 className="text-xl text-white font-semibold 2xl:text-2xl">
+        UI Design
+      </h3>
+
+      <div className="prolang  flex justify-start flex-wrap gap-5 mt-5 2xl:hidden">
+        <div className="figma p-2 flex flex-col items-center gap-6">
+          <Image
+            src="/Skills/figmaskills.png"
+            alt="loading.."
+            loading="lazy"
+            width={45}
+            height={45}
+          />
+
+          <p className="text-white text-sm font-semibold">Figma</p>
+        </div>
+        <div className="canva p-2 flex flex-col items-center gap-7">
+          <Image
+            src="/Skills/canvaskills.png"
+            alt="loading.."
+            loading="lazy"
+            width={45}
+            height={45}
+          />
+
+          <p className="text-white text-sm font-semibold">Canva</p>
+        </div>
+      </div>
+
+      <div className="prolang hidden 2xl:flex justify-start gap-20 mt-5">
         <div className="figma p-5 flex flex-col items-center gap-2">
           <Image
             src="/Skills/figmaskills.png"
