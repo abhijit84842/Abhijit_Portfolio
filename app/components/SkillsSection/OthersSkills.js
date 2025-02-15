a0730ba6bd4d158d3c67e9b45f36158bdaf9ec31
Abhijit Das <abhijitd721201@gmail.com>
2025-02-15 23:30:43 +0530

    skills section make responsive for ipad pro and iphone 12 pro

--- app/components/SkillsSection/OthersSkills.js
+++ app/components/SkillsSection/OthersSkills.js
@@ -3,9 +3,26 @@ import React from "react";
 
 const OthersSkills = () => {
   return (
-    <div className="programming-lang bg-zinc-800 p-5  mb-10 mx-5 rounded-lg">
-      <h3 className="text-2xl text-white font-semibold">Others Skills</h3>
-      <div className="prolang  flex justify-start gap-20 mt-5">
+    <div className="programming-lang bg-zinc-800 p-2  my-5 mx-2 rounded-lg 2xl:my-10 2xl:p-5 2xl:mx-5">
+      <h3 className="text-xl text-white font-semibold 2xl:text-2xl">
+        Others Skills
+      </h3>
+
+      <div className="prolang  flex justify-start flex-wrap gap-5 mt-5 2xl:hidden">
+        <div className="figma p-2 flex flex-col items-center gap-5">
+          <Image
+            src="/Skills/gitskills.png"
+            alt="loading.."
+            loading="lazy"
+            width={45}
+            height={45}
+          />
+
+          <p className="text-white text-sm font-semibold">Git & GitHub</p>
+        </div>
+      </div>
+
+      <div className="prolang hidden 2xl:flex justify-start gap-20 mt-5">
         <div className="figma p-5 flex flex-col items-center gap-2">
           <Image
             src="/Skills/gitskills.png"
--- app/components/SkillsSection/ProfessionalSkills.js
+++ app/components/SkillsSection/ProfessionalSkills.js
@@ -3,13 +3,63 @@ import React from "react";
 
 const ProfessionalSkills = () => {
   return (
-    <div className=" mt-10  p-5 ">
+    <div className=" mt-5  p-2 2xl:p-5 2xl:mt-10">
       <div className="p-2 ">
-        <h2 className="text-[#FB2A2A] text-3xl font-semibold">
+        <h2 className="text-[#FB2A2A] text-xl font-semibold 2xl:text-3xl">
           Professional Skills
         </h2>
       </div>
-      <div className="bg-zinc-800 flex justify-start gap-20 my-5 p-5 rounded-lg">
+
+      <div className="bg-zinc-800 flex justify-start flex-wrap gap-5 my-5 p-2 rounded-lg 2xl:hidden">
+        <div className=" p-2 flex flex-col items-center gap-5">
+          <Image
+            src="/Skills/communicationskills.png"
+            alt="loading.."
+            loading="lazy"
+            width={45}
+            height={45}
+          />
+
+          <p className="text-sm text-white font-semibold">
+            Communication Skill
+          </p>
+        </div>
+        <div className=" p-2 flex flex-col items-center gap-5">
+          <Image
+            src="/Skills/teamworkskills.png"
+            alt="loading.."
+            loading="lazy"
+            width={45}
+            height={45}
+          />
+
+          <p className="text-sm text-white font-semibold">Team Work</p>
+        </div>
+        <div className="p-2 flex flex-col items-center gap-7 ">
+          <Image
+            src="/Skills/problemskills.png"
+            alt="loading.."
+            loading="lazy"
+            width={50}
+            height={50}
+          />
+
+          <p className="text-sm text-white font-semibold">Problem Solving</p>
+        </div>
+        <div className=" p-2 flex flex-col items-center gap-7 ">
+          <Image
+            src="/Skills/creativityskills.png"
+            alt="loading.."
+            loading="lazy"
+            width={50}
+            height={50}
+          />
+
+          <p className="text-sm text-white font-semibold">Creativity</p>
+        </div>
+      </div>
+
+      <div className="hidden   2xl:flex justify-start gap-20 bg-zinc-800 my-5 p-5 rounded-lg">
         <div className=" p-5 flex flex-col items-center gap-5">
           <Image
             src="/Skills/communicationskills.png"
