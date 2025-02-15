69de8d35c27bd5aa15ae892afa437fc69d99ac72
Abhijit Das <abhijitd721201@gmail.com>
2025-02-15 18:19:51 +0530

    make responsive exprience comp for iphone 12 pro

--- app/components/ExperienceSection/Experience.js
+++ app/components/ExperienceSection/Experience.js
@@ -2,26 +2,28 @@ import React from "react";
 
 const Experience = () => {
   return (
-    <div className="experience-container my-20" id="exprience">
-      <h1 className="text-[#F61E1E] flex justify-center text-4xl font-semibold">
+    <div className="experience-container my-10 p-5 2xl:p-0" id="exprience">
+      <h1 className="text-[#F61E1E] flex justify-center text-3xl font-semibold 2xl:text-4xl">
         My Experience
       </h1>
-      <div className="company flex justify-between  mx-20 mt-10 ">
+      <div className="company    mt-5 2xl:mx-20 2xl:mt-10 2xl:flex justify-between">
         <div className="company-name">
-          <p className="text-3xl text-white font-semibold">
+          <p className="text-sm text-white font-semibold 2xl:text-3xl">
             Sikharthy Infotech Pvt.Ltd <span className="text-[#F61E1E]">|</span>{" "}
             <span className="text-[#0B44FF]">Frontend Developer Intern</span>{" "}
           </p>
         </div>
-        <div className="date">
-          <p className="text-2xl text-[#F61E1E]">Jan 2023 - June 2023</p>
+        <div className="date mt-2 2xl:mt-0">
+          <p className="text-sm text-[#F61E1E] 2xl:text-2xl">
+            Jan 2023 - June 2023
+          </p>
         </div>
       </div>
-      <div className="experience-text  w-[80%] bg-stone-700  mt-5 ml-[6rem] rounded-md">
-        <ul className="ml-5 p-5 text-white list-disc leading-10">
+      <div className="experience-text  bg-stone-700 rounded-md mt-5 w-full  2xl:w-[80%] 2xl:mt-10 2xl:ml-[6rem]">
+        <ul className="ml-2 p-3 text-white list-disc leading-5    2xl:ml-5 2xl:p-5 2xl:leading-10">
           <li className="my-3 ">
             {" "}
-            <p className="text-xl">
+            <p className="text-sm  2xl:text-xl">
               {" "}
               I gained hands-on experience in front-end development, working on
               multiple projects that involved creating dynamic, user-friendly
@@ -32,15 +34,15 @@ const Experience = () => {
             </p>
           </li>
           <li className="my-3">
-            <p className="text-xl">
+            <p className="text-sm text-ellipsis 2xl:text-xl">
               Directed design, writing and production of page content to fulfill
               project demands and satisfy customer needs. Reviewed.
             </p>{" "}
           </li>
           <li className="my-3">
-            <p className="text-xl">
+            <p className="text-sm 2xl:text-xl">
               {" "}
-              <span className="text-2xl font-semibold">
+              <span className="text-lg font-semibold 2xl:text-2xl">
                 Technology Used :-{" "}
               </span>{" "}
               React + Redux Toolkit , Next Js , HTML5, CSS3 , JavaScript,
@@ -48,25 +50,25 @@ const Experience = () => {
             </p>
           </li>
           <li>
-            <div className="flex">
-              <p className="text-2xl font-semibold">Tools :- </p>
-              <ul className="list-disc bg-stone-800 ml-20 mt-5 p-5 rounded-md">
+            <div className=" 2xl:flex">
+              <p className="text-lg font-semibold 2xl:text-2xl">Tools :- </p>
+              <ul className="list-disc bg-stone-800 ml-0 mt-2 p-5 rounded-md 2xl:ml-20 2xl:p-5 2xl:mt-5">
                 <li className="my-2">
-                  <p className="text-lg">
+                  <p className="text-sm 2xl:text-lg">
                     {" "}
-                    <span className="text-xl font-semibold">UI & UX :- </span>
+                    <span className="text-lg font-semibold 2xl:text-xl">UI & UX :- </span>
                     Figma & Canva
                   </p>
                 </li>
                 <li className="my-2">
-                  <p className="text-lg">
-                    <span className="text-xl font-semibold">IDE Use :- </span>VS
+                  <p className="text-sm 2xl:text-lg">
+                    <span className="text-lg font-semibold 2xl:text-xl">IDE Use :- </span>VS
                     Code , stackBlitz
                   </p>
                 </li>
                 <li className="my-2">
-                  <p className="text-lg">
-                    <span className="text-xl font-semibold">
+                  <p className="text-sm 2xl:text-lg">
+                    <span className="text-lg font-semibold 2xl:text-xl">
                       For Debugging :-{" "}
                     </span>
                     React Developer Tool , GitHub Copilot etc .
