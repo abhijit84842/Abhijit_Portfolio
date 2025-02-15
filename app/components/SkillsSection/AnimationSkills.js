45ac9eeb5285f9c751f6d158faaaacdc7b0d3bef
Abhijit Das <abhijitd721201@gmail.com>
2025-02-15 23:14:35 +0530

    make animation skills mobile and tab responsive

--- app/components/SkillsSection/AnimationSkills.js
+++ app/components/SkillsSection/AnimationSkills.js
@@ -3,9 +3,48 @@ import React from "react";
 
 const AnimationSkills = () => {
   return (
-    <div className="programming-lang bg-zinc-800 p-5  mb-10 mx-5 rounded-lg">
-      <h3 className="text-2xl text-white font-semibold">Animation</h3>
-      <div className="prolang  flex justify-start gap-20 mt-5">
+    <div className=" bg-zinc-800 p-2  my-5 mx-2 rounded-lg 2xl:my-10 2xl:mx-5 2xl:p-5">
+      <h3 className="text-xl text-white font-semibold 2xl:text-2xl">
+        Animation
+      </h3>
+
+      <div className="prolang  flex justify-start flex-wrap gap-5 mt-5 2xl:hidden">
+        <div className="gsap   h-[7rem] p-2 flex flex-col items-center gap-5">
+          <Image
+            src="/Skills/gsapskills.png"
+            alt="loading.."
+            loading="lazy"
+            width={50}
+            height={50}
+          />
+
+          <p className="text-white text-sm font-semibold">GSAP</p>
+        </div>
+        <div className="swiper  h-[7rem] p-2 flex flex-col items-center gap-8">
+          <Image
+            src="/Skills/swiperskills.png"
+            alt="loading.."
+            loading="lazy"
+            width={50}
+            height={50}
+          />
+
+          <p className="text-white text-sm font-semibold">Swiper Js</p>
+        </div>
+        <div className="canva  h-[7rem] p-2 flex flex-col items-center gap-9 ">
+          <Image
+            src="/Skills/scrollskills.png"
+            alt="loading.."
+            loading="lazy"
+            width={70}
+            height={70}
+          />
+
+          <p className="text-white text-sm font-semibold">Scroll Trigger</p>
+        </div>
+      </div>
+
+      <div className="prolang hidden 2xl:flex justify-start gap-20 mt-5">
         <div className="gsap  w-[7rem] min-w-[7rem] h-[11rem] p-5 flex flex-col items-center gap-8">
           <Image
             src="/Skills/gsapskills.png"
