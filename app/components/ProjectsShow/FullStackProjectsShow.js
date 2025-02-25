ae2e20896e04257e7d0ddee6f5b2ec38b4873118
Abhijit Das <abhijitd721201@gmail.com>
2025-02-25 13:00:24 +0530

    make mobile responsive project show sec

--- app/components/ProjectsShow/FullStackProjectsShow.js
+++ app/components/ProjectsShow/FullStackProjectsShow.js
@@ -16,93 +16,185 @@ import Image from "next/image";
 
 const FullStackProjectsShow = () => {
   return (
-    <div className="sub-container bg-slate-800 p-5 my-10 rounded-lg">
+    <div className="sub-container bg-slate-800 rounded-lg p-2 my-5 lg:my-10 lg:p-3 2xl:p-5 2xl:my-10 ">
       <div className="fullstack-projects-box">
-        <h2 className="mt-5 text-3xl mb-5 text-red-500 font-semibold">
+        <h2 className="text-red-500 font-semibold my-2 text-xl lg:text-3xl lg:my-5 2xl:mt-5 2xl:text-3xl 2xl:mb-5">
           Full Stack Projects
         </h2>
+        <div className="swiper-container  lg:hidden 2xl:hidden">
+          <Swiper
+            slidesPerView={1}
+            spaceBetween={20}
+            freeMode={true}
+            pagination={{
+              clickable: true,
+            }}
+            //  autoplay={{
+            //   delay:4500,
+            //   disableOnInteraction:false
+            //  }}
+            //  loop={true}
+            modules={[FreeMode, Pagination, Autoplay]}
+            className={styles.firstSwiper}
+          >
+            <SwiperSlide>
+              {" "}
+              <div className="crad  bg-slate-200 w-[20rem] p-3 rounded-lg h-[35rem] mx-2 mb-2">
+                <Link href="/pages/projectall/overview/cityslingoverview">
+                  <Image
+                    src="/ProjectShow/CitySlingImg.png"
+                    alt="loading...."
+                    loading="lazy"
+                    width={500}
+                    height={500}
+                    className="transition-transform duration-300 ease-in-out hover:scale-110"
+                  />
+                  <p className="text-red-500 text-xl mt-2 font-semibold mb-2 mt-3">
+                    CitySling Bag E-com Website
+                  </p>
+                </Link>
+                <p className="text-black text-sm">
+                  Full-stack e-commerce application is designed for a city sling
+                  bag store, built using EJS for templating, Node.js for backend
+                  logic, and Tailwind CSS for modern styling. The application
+                  uses MongoDB Atlas data storage and includes authentication
+                  with JWT and password encryption with bcrypt to protect user
+                  data. 
+                  <span>
+                    <Link
+                      className="text-blue-700 text-sm"
+                      href={"/pages/projectall/overview/cityslingoverview"}
+                    >
+                      See more..
+                    </Link>
+                  </span>
+                </p>
+              </div>
+            </SwiperSlide>
 
-        <Swiper
-          slidesPerView={3}
-          spaceBetween={30}
-          freeMode={true}
-          pagination={{
-            clickable: true,
-          }}
-          //  autoplay={{
-          //   delay:4500,
-          //   disableOnInteraction:false
-          //  }}
-          //  loop={true}
-          modules={[FreeMode, Pagination, Autoplay]}
-          className={styles.firstSwiper}
-        >
-          <SwiperSlide>
-            {" "}
-            <div className="crad bg-slate-200 w-[20rem] p-4 rounded-lg h-[35rem]">
-              <Link href="/pages/projectall/overview/cityslingoverview">
-                <Image
-                  src="/ProjectShow/CitySlingImg.png"
-                  alt="loading...."
-                  loading="lazy"
-                  width={500}
-                  height={500}
-                  className="transition-transform duration-300 ease-in-out hover:scale-110"
-                />
-                <p className="text-red-500 text-xl mt-2 font-semibold mb-2 mt-3">
-                  CitySling Bag E-com Website
+            <SwiperSlide>
+              {" "}
+              <div className="crad  bg-slate-200 w-[20rem] p-3 rounded-lg h-[35rem] ">
+                <Link href="/pages/projectall/overview/foodplazaoverview">
+                  <Image
+                    src="/ProjectShow/FoodPlaza.png"
+                    alt="loading..."
+                    loading="lazy"
+                    width={500}
+                    height={500}
+                    className="transition-transform duration-300 ease-in-out hover:scale-110"
+                  />
+                  <p className="text-red-500 text-xl mt-2 font-semibold mb-2 mt-3">
+                    Food Plaza Restaurant Website
+                  </p>
+                </Link>
+                <p className="text-black text-sm">
+                  Food Plaza, showcases a wide variety of meal options,
+                  including breakfast, lunch, and dinner, to provide users with
+                  a complete dining experience. Developed with the MERN stack
+                  (MongoDB, Express, React, Node.js).
+                  <span>
+                    <Link
+                      className="text-blue-700 text-sm"
+                      href={"/pages/projectall/overview/foodplazaoverview"}
+                    >
+                      See more..
+                    </Link>
+                  </span>
                 </p>
-              </Link>
-              <p className="text-black text-sm">
-                Full-stack e-commerce application is designed for a city sling
-                bag store, built using EJS for templating, Node.js for backend
-                logic, and Tailwind CSS for modern styling. The application uses
-                MongoDB Atlas data storage and includes authentication with JWT
-                and password encryption with bcrypt to protect user data.
-                <span>
-                  <Link
-                    className="text-blue-700 text-lg"
-                    href={"/pages/projectall/overview/cityslingoverview"}
-                  >
-                    See more..
-                  </Link>
-                </span>
-              </p>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            {" "}
-            <div className="crad bg-slate-200 w-[20rem] p-4 rounded-lg h-[35rem]">
-              <Link href="/pages/projectall/overview/foodplazaoverview">
-                <Image
-                  src="/ProjectShow/FoodPlaza.png"
-                  alt="loading..."
-                  loading="lazy"
-                  width={500}
-                  height={500}
-                  className="transition-transform duration-300 ease-in-out hover:scale-110"
-                />
-                <p className="text-red-500 text-xl mt-2 font-semibold mb-2 mt-3">
-                  Food Plaza Restaurant Website
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </div>
+
+
+
+
+        <div className="swiper-container hidden lg:block 2xl:block ">
+          <Swiper
+            slidesPerView={3}
+            spaceBetween={30}
+            freeMode={true}
+            pagination={{
+              clickable: true,
+            }}
+            //  autoplay={{
+            //   delay:4500,
+            //   disableOnInteraction:false
+            //  }}
+            //  loop={true}
+            modules={[FreeMode, Pagination, Autoplay]}
+            className={styles.firstSwiper}
+          >
+            <SwiperSlide>
+              {" "}
+              <div className="crad  bg-slate-200 w-[20rem] p-4 rounded-lg h-[35rem]">
+                <Link href="/pages/projectall/overview/cityslingoverview">
+                  <Image
+                    src="/ProjectShow/CitySlingImg.png"
+                    alt="loading...."
+                    loading="lazy"
+                    width={500}
+                    height={500}
+                    className="transition-transform duration-300 ease-in-out hover:scale-110"
+                  />
+                  <p className="text-red-500 text-xl mt-2 font-semibold mb-2 mt-3">
+                    CitySling Bag E-com Website
+                  </p>
+                </Link>
+                <p className="text-black text-sm">
+                  Full-stack e-commerce application is designed for a city sling
+                  bag store, built using EJS for templating, Node.js for backend
+                  logic, and Tailwind CSS for modern styling. The application
+                  uses MongoDB Atlas data storage and includes authentication
+                  with JWT and password encryption with bcrypt to protect user
+                  data.
+                  <span>
+                    <Link
+                      className="text-blue-700 text-lg"
+                      href={"/pages/projectall/overview/cityslingoverview"}
+                    >
+                      See more..
+                    </Link>
+                  </span>
                 </p>
-              </Link>
-              <p className="text-black text-sm">
-                Food Plaza, showcases a wide variety of meal options, including
-                breakfast, lunch, and dinner, to provide users with a complete
-                dining experience. Developed with the MERN stack (MongoDB,
-                Express, React, Node.js).
-                <span>
-                  <Link
-                    className="text-blue-700 text-lg"
-                    href={"/pages/projectall/overview/foodplazaoverview"}
-                  >
-                    See more..
-                  </Link>
-                </span>
-              </p>
-            </div>
-          </SwiperSlide>
-        </Swiper>
+              </div>
+            </SwiperSlide>
+
+            <SwiperSlide>
+              {" "}
+              <div className="crad  bg-slate-200 w-[20rem] p-4 rounded-lg h-[35rem] ">
+                <Link href="/pages/projectall/overview/foodplazaoverview">
+                  <Image
+                    src="/ProjectShow/FoodPlaza.png"
+                    alt="loading..."
+                    loading="lazy"
+                    width={500}
+                    height={500}
+                    className="transition-transform duration-300 ease-in-out hover:scale-110"
+                  />
+                  <p className="text-red-500 text-xl mt-2 font-semibold mb-2 mt-3">
+                    Food Plaza Restaurant Website
+                  </p>
+                </Link>
+                <p className="text-black text-sm">
+                  Food Plaza, showcases a wide variety of meal options,
+                  including breakfast, lunch, and dinner, to provide users with
+                  a complete dining experience. Developed with the MERN stack
+                  (MongoDB, Express, React, Node.js).
+                  <span>
+                    <Link
+                      className="text-blue-700 text-lg"
+                      href={"/pages/projectall/overview/foodplazaoverview"}
+                    >
+                      See more..
+                    </Link>
+                  </span>
+                </p>
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </div>
       </div>
     </div>
   );
--- app/pages/projectall/page.js
+++ app/pages/projectall/page.js
@@ -7,8 +7,8 @@ import React from "react";
 const page = () => {
   return (
     <div className="allprojects-main-container">
-      <h1 className="flex justify-center text-5xl font-semibold mt-10">All Projects</h1>
-      <div className=" bg-zinc-900 mx-10 rounded-lg p-4 my-10">
+      <h1 className=" font-semibold flex justify-center mt-5 text-3xl lg:text-5xl 2xl:flex 2xl:justify-center 2xl:text-5xl 2xl:mt-10">All Projects</h1>
+      <div className=" bg-zinc-900 rounded-lg mx-2 p-2 my-6 lg:p-4 2xl:mx-10  2xl:p-4 ">
       <FullStackProjectsShow/>
       <FrontendProjectsShow/>
       <OthersProjectsShow/>
